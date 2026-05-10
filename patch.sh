56cd93548701b639ba5a5369daa01f1ca6ae5558
Lucas2011wastaken <shengjiongyuan@outlook.com>
2026-05-10 03:23:49 +0000

    ADD A `.sh` SCRIPT FOR QUICK APPLY

---
+++ patch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp ./dots/.config/quickshell/ii/services/Ai.qml \
+   ~/.config/quickshell/ii/services/Ai.qml
+
+cp ./dots/.config/quickshell/ii/services/ai/AiMessageData.qml \
+   ~/.config/quickshell/ii/services/ai/AiMessageData.qml
+
+cp ./dots/.config/quickshell/ii/services/ai/OpenAiApiStrategy.qml \
+   ~/.config/quickshell/ii/services/ai/OpenAiApiStrategy.qml
+
+cp ./dots/.config/quickshell/ii/services/ai/MistralApiStrategy.qml \
+   ~/.config/quickshell/ii/services/ai/MistralApiStrategy.qml
+   
+cp ./dots/.config/quickshell/ii/modules/ii/sidebarLeft/AiChat.qml \
+   ~/.config/quickshell/ii/modules/ii/sidebarLeft/AiChat.qml
