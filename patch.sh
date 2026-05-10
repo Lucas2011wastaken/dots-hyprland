fdecc1830653ebc11239658f903163289f7aa48c
Lucas2011wastaken <shengjiongyuan@outlook.com>
2026-05-10 15:49:11 +0800

    MODIFIED patch.sh

--- patch.sh
+++ patch.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-cp ./dots/.config/quickshell/ii/services/Ai.qml \
+cp -i ./dots/.config/quickshell/ii/services/Ai.qml \
    ~/.config/quickshell/ii/services/Ai.qml
 
-cp ./dots/.config/quickshell/ii/services/ai/AiMessageData.qml \
+cp -i ./dots/.config/quickshell/ii/services/ai/AiMessageData.qml \
    ~/.config/quickshell/ii/services/ai/AiMessageData.qml
 
-cp ./dots/.config/quickshell/ii/services/ai/OpenAiApiStrategy.qml \
+cp -i ./dots/.config/quickshell/ii/services/ai/OpenAiApiStrategy.qml \
    ~/.config/quickshell/ii/services/ai/OpenAiApiStrategy.qml
 
-cp ./dots/.config/quickshell/ii/services/ai/MistralApiStrategy.qml \
+cp -i ./dots/.config/quickshell/ii/services/ai/MistralApiStrategy.qml \
    ~/.config/quickshell/ii/services/ai/MistralApiStrategy.qml
    
-cp ./dots/.config/quickshell/ii/modules/ii/sidebarLeft/AiChat.qml \
+cp -i ./dots/.config/quickshell/ii/modules/ii/sidebarLeft/AiChat.qml \
    ~/.config/quickshell/ii/modules/ii/sidebarLeft/AiChat.qml
+
+cp -i -- ./dots/.config/quickshell/ii/translations/*.json \
+    ~/.config/quickshell/ii/translations/
